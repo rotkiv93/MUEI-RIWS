92d02a1501a30493768fe29970fc1a23fec5bfe3
Victor <victorlamas93@gmail.com>
2019-10-06 21:20:13 +0100

    Now iterates through items, TODO: parse and save

--- Crawler/tutorial/tutorial/spiders/quotes_spider.py
+++ Crawler/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,29 +5,36 @@ from scrapy.linkextractors import LinkExtractor
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+
     start_urls = [
-        'https://www.prozis.com/es/es/alimentacion-saludable/barritas-y-snacks-para-llevar/barritas//es/es/alimentacion-saludable/barritas-y-snacks-para-llevar/muffins']
+        'https://www.prozis.com/es/es/alimentacion-saludable/barritas-y-snacks-para-llevar/muffins']
     
 
     rules = [
-        Rule(LinkExtractor(allow=(), restrict_xpaths=(
-            "//a[@class='w-100 h-100 m-0 p-0']")), follow=True),
-        Rule(LinkExtractor(
-            allow=("//a[@class='w-100 h-100 m-0 p-0']")), callback="parse_item")
+        Rule(LinkExtractor(allow=(),
+            restrict_xpaths=('//a[@class="w-100 h-100 m-0 p-0"]/@href')
+        ) ,callback="parse_item" ),
+
     ]
 
-    def parse_item (self, response):
+    def parse(self, response):
+        baseUrl = "https://www.prozis.com/es/es/prozis/"
+
+        urls = response.xpath(
+            "//a[re:test(@class, 'w-100 h-100 m-0 p-0')]/@href").extract()
+        
+        for f in urls:
+            new_url = baseUrl + f
+            yield scrapy.Request(url=new_url, callback=self.parse_item)
+
+        
+    def parse_item(self, response):
+        self.log("holis hago lo que hay que hacer")
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-
-    def parse(self, response):
-        item_url = response.xpath(
-            "//a[@class='w-100 h-100 m-0 p-0']/@href").extract_first()
-        self.logger.error(item_url)
-        
 
 
         
