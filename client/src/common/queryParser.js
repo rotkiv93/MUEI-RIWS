77b094a29b70b7bb5e25beca5198c5ba22294921
Victor <victorlamas93@gmail.com>
2019-11-19 18:48:38 +0100

    Example of query

--- client/src/common/queryParser.js
+++ client/src/common/queryParser.js
@@ -1,3 +1,16 @@
+/*
+search: function() {
+            var body = {
+                    size: 200,
+                    from: 0,
+                    query: {
+                        match: {
+                            name: this.query
+                        }
+                    }
+                }
+ */
+
 export default function queryParser(filters) {
   return JSON.stringify(filters);
 }
